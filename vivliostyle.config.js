453c5d021a7a344df86969e943cb992e6b209b97
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-07-15 07:30:26 +0900

    fix: 依存先のバージョン、パッケージの名前を修正

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,13 +1,11 @@
 module.exports = {
   language: 'en',
   theme: ['node_modules/@vivliostyle/theme-base', '.'],
-  entry: ['example/default.md'],
-  workspaceDir: '.vivliostyle',
+  entry: [
+    'example/title.md',
+    'example/chapter1.md'
+  ],
   output: [
     'book.pdf',
-    {
-      path: './book',
-      format: 'webpub',
-    },
   ],
 };
