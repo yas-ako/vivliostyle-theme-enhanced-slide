9a03b37d557f6d92a7b6f510c9bbdd17aa77ab51
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-07-17 00:37:51 +0900

    feat: 表紙, セクション, 各スライドのタイトルを実装

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -3,7 +3,8 @@ module.exports = {
   theme: ['node_modules/@vivliostyle/theme-base', '.'],
   entry: [
     'example/_title.md',
-    'example/chapter1.md'
+    'example/chapter1.md',
+    'example/chapter2.md'
   ],
   output: [
     'book.pdf',
