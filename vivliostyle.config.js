296faf1a47af4e705b796b4ce1fd37326183633e
yas-ako <105139975+yas-ako@users.noreply.github.com>
2025-07-19 14:48:55 +0900

    fix: 数式の説明用mdをconfigについか

--- vivliostyle.config.js
+++ vivliostyle.config.js
@@ -1,10 +1,13 @@
+// @ts-check
+/** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
 module.exports = {
   language: 'ja',
   theme: ['node_modules/@vivliostyle/theme-base', '.'],
   entry: [
     'example/_title.md',
     'example/chapter1.md',
-    'example/chapter2.md'
+    'example/chapter2.md',
+    'example/chapter3.md'
   ],
   output: [
     'book.pdf',
